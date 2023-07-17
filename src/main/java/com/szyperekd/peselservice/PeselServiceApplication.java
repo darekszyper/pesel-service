3287a0d5c59763d4d883c244568c6061c1a47d29
Dariusz Szyperek <dariusz.szyperek@britenet.com.pl>
2023-07-17 14:58:29 +0200

    Initial commit

---
+++ src/main/java/com/szyperekd/peselservice/PeselServiceApplication.java
@@ -0,0 +1,13 @@
+package com.szyperekd.peselservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeselServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeselServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szyperekd/peselservice/PeselServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szyperekd.peselservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeselServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
