3e9e181d3405640d1f571f2749285ecd83dd7ef5
Dariusz Szyperek <dariusz.szyperek@britenet.com.pl>
2023-07-19 11:32:18 +0200

    feat: Swagger implemented

--- src/main/java/com/szyperekd/peselservice/controller/PeselController.java
+++ src/main/java/com/szyperekd/peselservice/controller/PeselController.java
@@ -6,10 +6,7 @@ import com.szyperekd.peselservice.exception.InvalidPeselException;
 import com.szyperekd.peselservice.service.PeselDecoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
