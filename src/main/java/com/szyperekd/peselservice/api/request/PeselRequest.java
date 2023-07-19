b7e6e859d5a31c64f6b4367a412410d6e20d88d5
Dariusz Szyperek <dariusz.szyperek@britenet.com.pl>
2023-07-19 15:05:51 +0200

    First tests implemented

--- src/main/java/com/szyperekd/peselservice/api/request/PeselRequest.java
+++ src/main/java/com/szyperekd/peselservice/api/request/PeselRequest.java
@@ -1,10 +1,11 @@
 package com.szyperekd.peselservice.api.request;
 
+import com.szyperekd.peselservice.service.PeselDecoder;
 import com.szyperekd.peselservice.service.PeselValidator;
 
 public record PeselRequest(String pesel) {
 
-    private static final PeselValidator peselValidator = new PeselValidator();
+    private static final PeselValidator peselValidator = new PeselValidator(new PeselDecoder());
 
     public PeselRequest {
         peselValidator.assertIsValid(pesel);
