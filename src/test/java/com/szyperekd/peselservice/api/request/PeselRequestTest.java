3ecb3f4a83dfc1fedf6fa4c834600dc7f39f818a
Dariusz Szyperek <dariusz.szyperek@britenet.com.pl>
2023-07-20 10:18:14 +0200

    PeselDecoderTest implemented

--- src/test/java/com/szyperekd/peselservice/api/request/PeselRequestTest.java
+++ src/test/java/com/szyperekd/peselservice/api/request/PeselRequestTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.stream.Stream;
 
@@ -13,12 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PeselRequestTest {
 
-
     @ParameterizedTest
     @MethodSource("provideCorrectPesels")
     @DisplayName("Should create valid object from correct pesel number")
-    void shouldCreateValidObjetOfPesel(String example) {
-        var pesel = new PeselRequest(example);
+    void shouldCreateValidObjetOfPesel(String peselRequest) {
+        // when
+        var pesel = new PeselRequest(peselRequest);
+
+        //then
         assertNotNull(pesel);
     }
 
@@ -36,22 +39,31 @@ class PeselRequestTest {
     }
 
     @ParameterizedTest
+    @NullSource
+    @DisplayName("Should throw InvalidPeselException when PESEL is null")
+    void shouldThrowInvalidPeselExceptionWhenPeselIsNull(String peselRequest) {
+        assertThrows(InvalidPeselException.class, () -> new PeselRequest(peselRequest));
+    }
+
+    @ParameterizedTest
     @MethodSource("provideInvalidPesels")
-    void should_throw_exception_if_given_pesel_is_invalid(String givenPesel) {
-        assertThrows(InvalidPeselException.class, () -> new PeselRequest(givenPesel));
+    @DisplayName("Should throw InvalidPeselException object when PESEL is invalid")
+    void shouldThrowInvalidPeselExceptionWhenPeselIsInvalid(String peselRequest) {
+        assertThrows(InvalidPeselException.class, () -> new PeselRequest(peselRequest));
     }
 
     private static Stream<Arguments> provideInvalidPesels() {
         return Stream.of(
                 Arguments.of("string"),
                 Arguments.of("85122-96612"),
-                Arguments.of("950510017595"),
+                Arguments.of("950510501759"),
                 Arguments.of(""),
                 Arguments.of("11111111111"),
                 Arguments.of("95051001755"),
+                Arguments.of("           "),
+                Arguments.of("04562266388"),
+                Arguments.of("04514566382"),
                 Arguments.of("01016000019")
         );
     }
-
-
 }
